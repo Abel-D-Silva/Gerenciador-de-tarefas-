4c7f873e018bf40d0a088c920fc7b638f84051c6
Abel D. Silva <codyauditore@gmail.com>
2022-09-16 18:43:18 -0300

    Update index.php

--- tarefas-tarefosas/index.php
+++ tarefas-tarefosas/index.php
@@ -2,11 +2,65 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerenciador de Tarefas</title>
 </head>
 <body>
-    <h1>Texto Aqui</h1>
+    
+<div class="container">
+
+<h1>Texto Aqui</h1>
+
+
+<div class="row">
+ <div class="col s12 m6">
+   <div class="card blue-grey darken-1">
+     <div class="card-content white-text">
+       <div class = "form" >
+            <form action = " " method = "get">
+                 <label for = "task_name"> </label>
+            </form>
+            <ul>
+                 <input type = "text" name = "task_name" placeholder = "Nome da Tarefa">
+                 <button type = "submit"> Cadastrar </button>
+       </div>
+       <div class = "separator">
+       </div>
+       <div class = "list-tasks">
+       </div >
+                 <li> Tarefa 1 </li>
+                 <li> Tarefa 1 </11>
+                 <li> Tarefa 1 </li>
+            </ul>
+     </div>
+     <div class="card-action">
+       <a href="#">This is a link</a>
+       <a href="#">This is a link</a>
+     </div>
+   </div>
+ </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+<!--JavaScript at end of body for optimized loading-->
+<script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
 </html>
